e2a6e656a5968742c4e1af10c159e00763bb6379
Blank <guoshaoxuan1@gmail.com>
2025-04-15 10:49:19 -0400

    end

--- Car.java
+++ Car.java
@@ -2,8 +2,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Car extends Actor
 {
-    public void act()
-    {
-        move(-3);
+    public void act() {
+        move(-10);
+        
+        if (getX() <= 0) {
+            resetCar();
+        }
+        
+        if (isTouching(Hero.class)){
+            SadFace sadFace = new SadFace();
+            getWorld().addObject (sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetCar(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
